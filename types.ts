1f915ea7a01adb911e2d557a40c97caf19ba1263
Ulf Dellbrügge <xintamosaik@gmail.com>
2026-03-23 21:58:03 +0100

    define fields

--- types.ts
+++ types.ts
@@ -1,10 +1,10 @@
-
 type TodoRow = {
   id: number;
   short: string;
   due_date: string;
-  cost_of_delay: number;
-  effort: number;
+  cost_of_delay: -2 | -1 | 0 | 1 | 2;
+  effort: "mins" | "hours" | "days" | "weeks" | "months";
+ 
 };
 
 export type { TodoRow };
